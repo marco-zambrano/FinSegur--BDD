cc03ef940c2b11d2184f56687e620129da55e539
marco <marcoadrianzambra@gmail.com>
2025-07-10 22:28:55 -0500

    fix: tables restriction

--- symmetric-ds/config/symmetric-config.sql
+++ symmetric-ds/config/symmetric-config.sql
@@ -10,82 +10,59 @@ values ('auditorias', 4, 1000, 1, 'Replicación de auditorias');
 insert into sym_channel (channel_id, processing_order, max_batch_size, enabled, description)
 values ('sucursales', 5, 1000, 1, 'Replicación de sucursales');
 
-
--- Grupos de Nodos
-insert into sym_node_group (node_group_id, description)
-values ('master', 'Grupo para el nodo master');
-insert into sym_node_group (node_group_id, description)
-values ('trans', 'Grupo para el nodo transaccional');
-insert into sym_node_group (node_group_id, description)
-values ('readonly', 'Grupo para el nodo de solo lectura');
-
--- Enlaces entre grupos (quién habla con quién)
--- Master <-> Trans
-insert into sym_node_group_link (source_node_group_id, target_node_group_id, data_event_action)
-values ('master', 'trans', 'P'); -- Push
-insert into sym_node_group_link (source_node_group_id, target_node_group_id, data_event_action)
-values ('trans', 'master', 'P'); -- Push
--- Master -> Readonly
-insert into sym_node_group_link (source_node_group_id, target_node_group_id, data_event_action)
-values ('master', 'readonly', 'P'); -- Push
-
-
 -- Triggers (qué tablas monitorear)
+-- NOTA: Para auditorias, el trigger solo debe existir en 'trans'
 insert into sym_trigger (trigger_id, source_table_name, channel_id, sync_on_update, sync_on_insert, sync_on_delete, last_update_time, create_time)
 values ('clientes_trigger', 'clientes', 'clientes', 1, 1, 1, current_timestamp, current_timestamp);
 insert into sym_trigger (trigger_id, source_table_name, channel_id, sync_on_update, sync_on_insert, sync_on_delete, last_update_time, create_time)
 values ('cuentas_trigger', 'cuentas', 'cuentas', 1, 1, 1, current_timestamp, current_timestamp);
 insert into sym_trigger (trigger_id, source_table_name, channel_id, sync_on_update, sync_on_insert, sync_on_delete, last_update_time, create_time)
 values ('movimientos_trigger', 'movimientos', 'movimientos', 1, 1, 1, current_timestamp, current_timestamp);
-insert into sym_trigger (trigger_id, source_table_name, channel_id, sync_on_update, sync_on_insert, sync_on_delete, last_update_time, create_time)
-values ('auditorias_trigger', 'auditorias', 'auditorias', 1, 1, 1, current_timestamp, current_timestamp);
+insert into sym_trigger (trigger_id, source_table_name, channel_id, sync_on_update, sync_on_insert, sync_on_delete, last_update_time, create_time, source_node_group_id)
+values ('auditorias_trigger', 'auditorias', 'auditorias', 1, 1, 1, current_timestamp, current_timestamp, 'trans'); -- <-- SOLO SE CREA EN EL GRUPO 'trans'
 insert into sym_trigger (trigger_id, source_table_name, channel_id, sync_on_update, sync_on_insert, sync_on_delete, last_update_time, create_time)
 values ('sucursales_trigger', 'sucursales', 'sucursales', 1, 1, 1, current_timestamp, current_timestamp);
 
+-- Habilitar replicación en cascada para que los datos que llegan al master se re-envíen
+UPDATE sym_trigger SET sync_on_incoming_batch = 1 WHERE trigger_id IN ('cuentas_trigger', 'auditorias_trigger');
 
 -- Routers (cómo dirigir los datos)
--- Regla general: Master envía todo a Trans y Readonly
 insert into sym_router (router_id, source_node_group_id, target_node_group_id, create_time, last_update_time)
-values ('master_to_all', 'master', 'trans', current_timestamp, current_timestamp);
+values ('master_to_trans', 'master', 'trans', current_timestamp, current_timestamp);
 insert into sym_router (router_id, source_node_group_id, target_node_group_id, create_time, last_update_time)
 values ('master_to_readonly', 'master', 'readonly', current_timestamp, current_timestamp);
-
--- Regla: Trans envía cuentas, movimientos y auditorias a Master
 insert into sym_router (router_id, source_node_group_id, target_node_group_id, create_time, last_update_time)
 values ('trans_to_master', 'trans', 'master', current_timestamp, current_timestamp);
-
 
 -- Enlazar Triggers con Routers
 -- Clientes: Master -> Trans, Master -> Readonly
 insert into sym_trigger_router (trigger_id, router_id, initial_load_order, last_update_time, create_time)
-values ('clientes_trigger', 'master_to_all', 100, current_timestamp, current_timestamp);
+values ('clientes_trigger', 'master_to_trans', 100, current_timestamp, current_timestamp);
 insert into sym_trigger_router (trigger_id, router_id, initial_load_order, last_update_time, create_time)
 values ('clientes_trigger', 'master_to_readonly', 100, current_timestamp, current_timestamp);
 
 -- Cuentas: Master <-> Trans, Master -> Readonly
 insert into sym_trigger_router (trigger_id, router_id, initial_load_order, last_update_time, create_time)
-values ('cuentas_trigger', 'master_to_all', 100, current_timestamp, current_timestamp);
+values ('cuentas_trigger', 'master_to_trans', 100, current_timestamp, current_timestamp);
 insert into sym_trigger_router (trigger_id, router_id, initial_load_order, last_update_time, create_time)
 values ('cuentas_trigger', 'master_to_readonly', 100, current_timestamp, current_timestamp);
 insert into sym_trigger_router (trigger_id, router_id, initial_load_order, last_update_time, create_time)
 values ('cuentas_trigger', 'trans_to_master', 100, current_timestamp, current_timestamp);
 
--- Movimientos: Master <-> Trans
+-- Movimientos: Master <-> Trans (NO va a Readonly)
 insert into sym_trigger_router (trigger_id, router_id, initial_load_order, last_update_time, create_time)
-values ('movimientos_trigger', 'master_to_all', 100, current_timestamp, current_timestamp);
+values ('movimientos_trigger', 'master_to_trans', 100, current_timestamp, current_timestamp);
 insert into sym_trigger_router (trigger_id, router_id, initial_load_order, last_update_time, create_time)
 values ('movimientos_trigger', 'trans_to_master', 100, current_timestamp, current_timestamp);
 
 -- Auditorias: Trans -> Master -> Readonly
--- Trans -> Master
 insert into sym_trigger_router (trigger_id, router_id, initial_load_order, last_update_time, create_time)
 values ('auditorias_trigger', 'trans_to_master', 100, current_timestamp, current_timestamp);
--- Master -> Readonly (para que lo que llega de Trans se reenvíe a Readonly)
 insert into sym_trigger_router (trigger_id, router_id, initial_load_order, last_update_time, create_time)
-values ('auditorias_trigger', 'master_to_readonly', 100, current_timestamp, current_timestamp);
+values ('auditorias_trigger', 'master_to_readonly', 100, current_timestamp, current_timestamp); -- Para la cascada
 
 -- Sucursales: Replicar a todos desde master (como default)
 insert into sym_trigger_router (trigger_id, router_id, initial_load_order, last_update_time, create_time)
-values ('sucursales_trigger', 'master_to_all', 100, current_timestamp, current_timestamp);
+values ('sucursales_trigger', 'master_to_trans', 100, current_timestamp, current_timestamp);
 insert into sym_trigger_router (trigger_id, router_id, initial_load_order, last_update_time, create_time)
 values ('sucursales_trigger', 'master_to_readonly', 100, current_timestamp, current_timestamp);
